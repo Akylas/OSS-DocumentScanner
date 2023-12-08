0c97e3a3c04bcac2cf7ee6789b4abe278fc5c158
farfromrefuge <>
2023-12-08 22:00:17 +0100

    chore: popover

---
+++ app/components/PopoverBackgroundView.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+    import { colors } from '~/variables';
+</script>
+
+<script lang="ts">
+    let colorSurfaceContainer = $colors.colorSurfaceContainer;
+    // technique for only specific properties to get updated on store change
+    $: ({ colorSurfaceContainer } = $colors);
+    export let backgroundColor = colorSurfaceContainer;
+</script>
+
+<gesturerootview>
+    <gridlayout {backgroundColor} borderRadius={10} elevation={4} margin={4} padding="0 10 0 10" {...$$restProps}>
+        <slot />
+    </gridlayout>
+</gesturerootview>
--- app/components/SliderPopover.svelte
+++ app/components/SliderPopover.svelte
@@ -1,17 +1,15 @@
 <script context="module" lang="ts">
     import { colors } from '~/variables';
+    import PopoverBackgroundView from './PopoverBackgroundView.svelte';
 </script>
 
 <script lang="ts">
-    // technique for only specific properties to get updated on store change
-    $: ({ colorSurfaceContainer } = $colors);
     export let icon: string = null;
     export let title: string = null;
     export let min = 0;
     export let max = 1;
     export let step = 0.01;
     export let value = 0;
-    export let backgroundColor = colorSurfaceContainer;
     export let onChange = null;
     export let formatter = (value) => value + '';
 
@@ -22,16 +20,14 @@
     }
 </script>
 
-<gridlayout>
-    <gridlayout {backgroundColor} borderRadius="4" columns="auto,*,auto" elevation="2" margin="2" padding="0 10 0 10" rows="auto,auto">
-        {#if icon}
-            <label class="icon-label" marginTop="0" text={icon} />
-        {/if}
-        {#if title}
-            <label col={1} marginTop="0" text={title} verticalTextAlignment="center" />
-        {/if}
-        <label row={1} text={formatter(min)} textAlignment="center" verticalTextAlignment="center" />
-        <label col={2} row={1} text={formatter(max)} textAlignment="center" verticalTextAlignment="center" />
-        <slider col={1} maxValue={max} minValue={min} row={1} stepSize={step} {value} on:valueChange={onValueChange} />
-    </gridlayout>
-</gridlayout>
+<PopoverBackgroundView columns="auto,*,auto" rows="auto,auto">
+    {#if icon}
+        <label class="icon-label" marginTop="0" text={icon} />
+    {/if}
+    {#if title}
+        <label col={1} marginTop="0" text={title} verticalTextAlignment="center" />
+    {/if}
+    <label row={1} text={formatter(min)} textAlignment="center" verticalTextAlignment="center" />
+    <label col={2} row={1} text={formatter(max)} textAlignment="center" verticalTextAlignment="center" />
+    <slider col={1} maxValue={max} minValue={min} row={1} stepSize={step} {value} on:valueChange={onValueChange} />
+</PopoverBackgroundView>
