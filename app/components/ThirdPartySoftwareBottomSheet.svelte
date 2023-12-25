8508df5cf2176146c956dd1bf345d21ea2fb9279
farfromrefuge <>
2023-12-25 17:07:07 +0100

    fix: better layout for 3rd party dependencies

--- app/components/ThirdPartySoftwareBottomSheet.svelte
+++ app/components/ThirdPartySoftwareBottomSheet.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
     import { Template } from 'svelte-native/components';
     import { openLink } from '~/utils/ui';
-    import { colors } from '~/variables';
+    import ListItemAutoSize from './ListItemAutoSize.svelte';
     // technique for only specific properties to get updated on store change
-    $: ({ colorPrimary } = $colors);
 
     const licences = require('~/licenses.json');
 
@@ -17,12 +16,9 @@
 </script>
 
 <gesturerootview rows="auto">
-    <collectionView id="trackingScrollView" class="bottomsheet" height="300" itemIdGenerator={(item, i) => i} {items} rowHeight="60">
+    <collectionView id="trackingScrollView" height="300" itemIdGenerator={(item, i) => i} {items}>
         <Template let:item>
-            <stacklayout padding="0 16 0 16" rippleColor={colorPrimary} verticalAlignment="middle" on:tap={() => onTap(item)}>
-                <label fontSize={17} maxLines={1} text={item.moduleName} verticalAlignment="top" />
-                <label color="#aaaaaa" fontSize={14} text={item.moduleUrl} verticalAlignment="bottom" />
-            </stacklayout>
+            <ListItemAutoSize subtitle={item.moduleUrl} title={item.moduleName} on:tap={() => onTap(item)} />
         </Template>
     </collectionView>
 </gesturerootview>
