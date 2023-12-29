9d885a649025173837e29e178020e44c52dd1656
farfromrefuge <>
2023-12-29 15:00:18 +0100

    chore(android): automatically generate proguard with plugins whitelist

--- scripts/createProguard.js
+++ scripts/createProguard.js
@@ -4,18 +4,33 @@ const buildToolsPath = path.join(process.argv[2], 'build-tools');
 const whitelist = fs.readFileSync(path.join(buildToolsPath, 'whitelist.mdg'), { encoding: 'utf-8' });
 const whitelistfilteredLines = [...new Set(whitelist.split('\n'))].filter((l) => l.length > 0 && !l.startsWith('//'));
 const blacklist = fs.readFileSync(path.join(buildToolsPath, 'blacklist.mdg'), { encoding: 'utf-8' });
-const blacklistfilteredLines = [...new Set(blacklist.split('\n'))].filter((l) => l.length > 0 && !l.startsWith('//'));
-const proguard_blacklist = require(path.join( process.argv[3],'Android', 'native-api-usage.json')).proguard_blacklist;
+// const blacklistfilteredLines = [...new Set(blacklist.split('\n'))].filter((l) => l.length > 0 && !l.startsWith('//'));
+const proguard_blacklist = require(path.join(process.argv[3], 'Android', 'native-api-usage.json')).proguard_blacklist;
+
+const dependencies = require(path.join(buildToolsPath, '../dependencies.json'));
+
+const proguard_whitelist = dependencies.reduce((acc, dep) => {
+    const depDir = path.resolve(path.join(buildToolsPath, '..', dep.directory));
+    const pluginNativeApiJSONPath = path.join(depDir, 'platforms', 'android', 'native-api-usage.json');
+    if (fs.existsSync(pluginNativeApiJSONPath)) {
+        const pluginNativeApiJSON = require(pluginNativeApiJSONPath);
+        if (pluginNativeApiJSON.proguard_whitelist) {
+            acc.push(...pluginNativeApiJSON.proguard_whitelist);
+        }
+    }
+
+    return acc;
+}, []);
 
 function getIdentifier(l) {
     let identifier = l;
     let suffix = '';
-        if (identifier.endsWith('*:*')) {
-            suffix =  '.**'
-            identifier = identifier.slice(0, -3);
-        }
-        return identifier.replace(':', '.').replace(/\*/g, '**') + suffix;
+    if (identifier.endsWith('*:*')) {
+        suffix = '.**';
+        identifier = identifier.slice(0, -3);
     }
+    return identifier.replace(':', '.').replace(/\*/g, '**') + suffix;
+}
 
 // const KEEP_PARAMS = '-keep,includedescriptorclasses,allowoptimization public class';
 const KEEP_PARAMS = '-keep,allowoptimization public class';
@@ -28,6 +43,7 @@ let data = `#-dontobfuscate
 -keep,includedescriptorclasses public interface com.tns.** { *; }
 
 ${whitelistfilteredLines
+    .concat(proguard_whitelist)
     .map((l) => {
         let identifier = l;
         if (identifier.endsWith('*:*')) {
@@ -41,10 +57,8 @@ ${whitelistfilteredLines
         }
         if (identifier.endsWith('*')) {
             identifier = identifier.slice(0, -1).replace(':', '.');
-
         } else {
             identifier = identifier.replace(':', '.');
-
         }
         return `${KEEP_PARAMS} ${identifier}** { public  *; }`;
     })
