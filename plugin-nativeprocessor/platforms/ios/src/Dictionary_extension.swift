38ebbe0a9ca20171590fa1d4b2d9572c02176905
Martin Guillon <dev@akylas.fr>
2024-02-02 22:02:04 +0100

    chore: cleanup

---
+++ plugin-nativeprocessor/platforms/ios/src/Dictionary_extension.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary_extension.swift
+//  nsdocumentscanner
+//
+//  Created by Martin Guillon on 02/02/2024.
+//  Copyright © 2024 NativeScript. All rights reserved.
+//
+
+import Foundation
