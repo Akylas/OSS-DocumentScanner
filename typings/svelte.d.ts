844ba27c96d9256ac595599f39f0e4d2216cd89a
Martin Guillon <dev@akylas.fr>
2024-03-22 14:46:18 +0100

    chore: svelte types

--- typings/svelte.d.ts
+++ typings/svelte.d.ts
@@ -4,7 +4,6 @@ declare module 'svelte/internal' {
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-qualifier
 declare namespace svelteNative.JSX {
     type Override<What, With> = Omit<What, keyof With> & With;
-    type ViewKeys = keyof TViewAttributes;
     type TViewAugmentedAttributes = Override<
         TViewAttributes,
         {
@@ -18,26 +17,42 @@ declare namespace svelteNative.JSX {
             // "on:shownModally"?: (args: ShownModallyData) => void;
         }
     >;
-    type ViewAndroidAttributes = {
-        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewIOSAttributes = {
-        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewAttributes = TViewAugmentedAttributes & ViewAndroidAttributes & ViewIOSAttributes;
 
-    interface ButtonAttributes {
-        variant?: string;
-        shape?: string;
-    }
-    interface ImageAttributes {
-        noCache?: boolean;
-        imageRotation?: number;
-        colorMatrix?: number[];
-        blurRadius?: number;
-        fadeDuration?: number;
-        'on:rotateAnimated'?: (args: EventData) => void;
-    }
+    type TButtonAugmentedAttributes = Override<
+        TButtonAttributes,
+        {
+            variant?: string;
+            shape?: string;
+        }
+    >;
+    type TImageAugmentedAttributes = Override<
+        TImageAttributes,
+        {
+            noCache?: boolean;
+            imageRotation?: number;
+            colorMatrix?: number[];
+            blurRadius?: number;
+            fadeDuration?: number;
+            contextOptions?: any;
+            'on:rotateAnimated'?: (args: EventData) => void;
+        }
+    >;
+    type TLabelAugmentedAttributes = Override<
+        TLabelAttributes,
+        {
+            autoFontSize?: boolean;
+            verticalTextAlignment?: string;
+            maxLines?: number;
+            minFontSize?: number;
+            maxFontSize?: number;
+            lineBreak?: string;
+            html?: string;
+            selectable?: boolean;
+            'ios:selectable'?: boolean;
+            onlinkTap?;
+            'on:linkTap'?;
+        }
+    >;
     interface SpanAttributes {
         verticalAlignment?: string;
         verticalTextAlignment?: string;
@@ -52,17 +67,24 @@ declare namespace svelteNative.JSX {
         keepScreenAwake?: boolean;
         screenBrightness?: number;
     }
-    interface LabelAttributes {
-        autoFontSize?: boolean;
-        verticalTextAlignment?: string;
-        maxLines?: number;
-        minFontSize?: number;
-        maxFontSize?: number;
-        lineBreak?: string;
-        html?: string;
-        selectable?: boolean;
-        'ios:selectable'?: boolean;
-        onlinkTap?;
-        'on:linkTap'?;
-    }
+    type ViewAttributes = TViewAugmentedAttributes & {
+        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[k];
+    } & {
+        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[k];
+    };
+    type ButtonAttributes = TButtonAugmentedAttributes & {
+        [K in keyof TButtonAugmentedAttributes as `ios:${K}`]: TButtonAugmentedAttributes[k];
+    } & {
+        [K in keyof TButtonAugmentedAttributes as `android:${K}`]: TButtonAugmentedAttributes[k];
+    };
+    type ImageAttributes = TImageAugmentedAttributes & {
+        [K in keyof TImageAugmentedAttributes as `ios:${K}`]: TImageAugmentedAttributes[k];
+    } & {
+        [K in keyof TImageAugmentedAttributes as `android:${K}`]: TImageAugmentedAttributes[k];
+    };
+    type LabelAttributes = TLabelAugmentedAttributes & {
+        [K in keyof TLabelAugmentedAttributes as `ios:${K}`]: TLabelAugmentedAttributes[k];
+    } & {
+        [K in keyof TLabelAugmentedAttributes as `android:${K}`]: TLabelAugmentedAttributes[k];
+    };
 }
