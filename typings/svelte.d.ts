a4ee94a6cce3e0452ab89ddc353c3aa2dfa1f9c4
farfromrefug <dev@akylas.fr>
2024-05-02 21:01:30 +0200

    chore: typings

--- typings/svelte.d.ts
+++ typings/svelte.d.ts
@@ -61,30 +61,38 @@ declare namespace svelteNative.JSX {
         stepSize?: number;
         trackBackgroundColor?: string;
     }
-    interface PageAttributes {
-        statusBarColor?: string;
-        screenOrientation?: string;
-        keepScreenAwake?: boolean;
-        screenBrightness?: number;
-    }
-    type ViewAttributes = TViewAugmentedAttributes & {
-        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[k];
+    type TPageAugmentedAttributes = Override<
+        TPageAttributes,
+        {
+            statusBarColor?: string;
+            screenOrientation?: string;
+            keepScreenAwake?: boolean;
+            screenBrightness?: number;
+        }
+    >;
+    type PageAttributes = TPageAugmentedAttributes & {
+        [K in keyof TPageAugmentedAttributes as `ios:${K}`]: TPageAugmentedAttributes[K];
     } & {
-        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[k];
+        [K in keyof TPageAugmentedAttributes as `android:${K}`]: TPageAugmentedAttributes[K];
+    };
+    type ViewAttributes = TViewAugmentedAttributes & {
+        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[K];
+    } & {
+        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[K];
     };
     type ButtonAttributes = TButtonAugmentedAttributes & {
-        [K in keyof TButtonAugmentedAttributes as `ios:${K}`]: TButtonAugmentedAttributes[k];
+        [K in keyof TButtonAugmentedAttributes as `ios:${K}`]: TButtonAugmentedAttributes[K];
     } & {
-        [K in keyof TButtonAugmentedAttributes as `android:${K}`]: TButtonAugmentedAttributes[k];
+        [K in keyof TButtonAugmentedAttributes as `android:${K}`]: TButtonAugmentedAttributes[K];
     };
     type ImageAttributes = TImageAugmentedAttributes & {
-        [K in keyof TImageAugmentedAttributes as `ios:${K}`]: TImageAugmentedAttributes[k];
+        [K in keyof TImageAugmentedAttributes as `ios:${K}`]: TImageAugmentedAttributes[K];
     } & {
-        [K in keyof TImageAugmentedAttributes as `android:${K}`]: TImageAugmentedAttributes[k];
+        [K in keyof TImageAugmentedAttributes as `android:${K}`]: TImageAugmentedAttributes[K];
     };
     type LabelAttributes = TLabelAugmentedAttributes & {
-        [K in keyof TLabelAugmentedAttributes as `ios:${K}`]: TLabelAugmentedAttributes[k];
+        [K in keyof TLabelAugmentedAttributes as `ios:${K}`]: TLabelAugmentedAttributes[K];
     } & {
-        [K in keyof TLabelAugmentedAttributes as `android:${K}`]: TLabelAugmentedAttributes[k];
+        [K in keyof TLabelAugmentedAttributes as `android:${K}`]: TLabelAugmentedAttributes[K];
     };
 }
