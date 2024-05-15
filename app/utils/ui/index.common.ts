7a582001718d7eee189994aaf1d4ddfa7f548909
Martin Guillon <dev@akylas.fr>
2024-05-15 22:12:38 +0200

    chore: use `Promise.allSettled` to wait on error

--- app/utils/ui/index.common.ts
+++ app/utils/ui/index.common.ts
@@ -206,9 +206,17 @@ function chunk<T>(array: T[], size) {
 export async function doInBatch<T, U>(array: T[], handler: (T, index: number) => Promise<U>, chunkSize: number = DEFAULT__BATCH_CHUNK_SIZE) {
     const chunks = chunk(array, chunkSize);
     const result: U[] = [];
-    const promises = chunks.map((s, i) => () => Promise.all(s.map((value, j) => handler(value, i * chunkSize + j))));
+    // we use allSettled to wait for all even if one failed
+    // that way we are sure we are finished on error and we can handle things correctly
+    const promises = chunks.map((s, i) => () => Promise.allSettled(s.map((value, j) => handler(value, i * chunkSize + j))));
     for (let index = 0; index < promises.length; index++) {
-        result.push(...(await promises[index]()));
+        const subResult = await promises[index]();
+        const firstError = subResult.find((s) => s.status === 'rejected')?.['reason'];
+        if (firstError) {
+            throw firstError;
+        }
+        const values: U[] = subResult.map((s) => s['value']);
+        result.push(...values);
     }
     return result;
 }
