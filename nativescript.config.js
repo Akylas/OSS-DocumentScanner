932b75fbda520c8571aa4eadce069183e934b7bb
farfromrefug <dev@akylas.fr>
2024-06-10 22:10:03 +0200

    chore: new cli options support

--- nativescript.config.js
+++ nativescript.config.js
@@ -2,6 +2,7 @@ const timelineEnabled = !!process.env['NS_TIMELINE'];
 const sentryEnabled = !!process.env['NS_SENTRY'];
 const loggingEnabled = !!process.env['NS_LOGGING'];
 const id = process.env['APP_ID'];
+const CARD_APP = id === 'com.akylas.cardwallet';
 module.exports = {
     ignoredNativeDependencies: [].concat(sentryEnabled ? [] : ['@nativescript-community/sentry']),
     id: id || 'com.akylas.documentscanner',
@@ -19,11 +20,16 @@ module.exports = {
         runtimePackageName: '@akylas/nativescript-ios-runtime'
     },
     android: {
+        plugins: {
+            'plugin-nativeprocessor': {
+                aarSuffix: CARD_APP ? '-cardwallet' : '-documentscanner'
+            }
+        },
         runtimePackageName: '@akylas/nativescript-android-runtime',
         gradleVersion: '8.6',
         markingMode: 'none',
         codeCache: true,
-        gradleArgs: id === 'com.akylas.cardwallet' ? ['-PwithQRCode'] : [],
+        gradleArgs: CARD_APP ? ['-PwithQRCode', '-PaarIgnoreFilter=**/*documentscanner*'] : ['-PaarIgnoreFilter=**/*cardwallet*'],
         enableMultithreadedJavascript: false,
         ...(loggingEnabled
             ? {
