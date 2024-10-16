a9651c1bf46b9be3bd1161fd5fef838ecbb10805
farfromrefug <dev@akylas.fr>
2024-10-16 18:55:28 +0200

    chore: ci

--- scripts/ci.prepare.sh
+++ scripts/ci.prepare.sh
@@ -2,8 +2,8 @@
 
 POSITIONAL_ARGS=()
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
+for i in "$@"; do
+  case $i in
     -p|--platform)
       PLATFORM="$2"
       shift # past argument
@@ -24,13 +24,9 @@ while [[ $# -gt 0 ]]; do
       exit 1
       ;;
     *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
       ;;
   esac
 done
-
-set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 echo "PLATFORM  = ${PLATFORM}"
 echo "VERSION   = ${VERSION}"
