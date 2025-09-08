0a4d7330f244e5b44035c578c698d676edc64edd
farfromrefug <dev@akylas.fr>
2025-09-08 14:42:06 +0200

    fix: make alert content scrollable #477

--- app/components/common/ExportPDFAlertOptions.svelte
+++ app/components/common/ExportPDFAlertOptions.svelte
@@ -19,32 +19,34 @@
     }
 </script>
 
-<gesturerootview padding="10 0 10 0" rows="auto">
-    <stacklayout>
-        <label class={'sectionBigHeader'} margin={10} text={lc('export_settings')} />
-        <FolderTextView text={folder} on:folder={onFolderSelect} />
-        <textfield
-            autocapitalizationType="none"
-            autocorrect={false}
-            hint={lc('pdf_filename')}
-            margin="5 10 5 10"
-            placeholder={lc('filename')}
-            returnKeyType="next"
-            text={filename}
-            variant="outline"
-            on:textChange={(e) => (filename = e['value'])} />
-        <SettingsSlider margin="5 10 5 10" max={100} min={0} onChange={(value) => (jpegQuality = value)} step={1} title={lc('jpeg_quality')} value={jpegQuality} />
-        <textfield
-            autocapitalizationType="none"
-            autocorrect={false}
-            hint={lc('optional_pdf_password')}
-            margin="5 10 5 10"
-            placeholder={lc('password')}
-            placeholderColor="gray"
-            returnKeyType="done"
-            secure={true}
-            text={password}
-            variant="outline"
-            on:textChange={(e) => (password = e['value'])} />
-    </stacklayout>
+<gesturerootview padding="10 0 10 0">
+    <scrollview>
+        <stacklayout>
+            <label class={'sectionBigHeader'} margin={10} text={lc('export_settings')} />
+            <FolderTextView text={folder} on:folder={onFolderSelect} />
+            <textfield
+                autocapitalizationType="none"
+                autocorrect={false}
+                hint={lc('pdf_filename')}
+                margin="5 10 5 10"
+                placeholder={lc('filename')}
+                returnKeyType="next"
+                text={filename}
+                variant="outline"
+                on:textChange={(e) => (filename = e['value'])} />
+            <SettingsSlider margin="5 10 5 10" max={100} min={0} onChange={(value) => (jpegQuality = value)} step={1} title={lc('jpeg_quality')} value={jpegQuality} />
+            <textfield
+                autocapitalizationType="none"
+                autocorrect={false}
+                hint={lc('optional_pdf_password')}
+                margin="5 10 5 10"
+                placeholder={lc('password')}
+                placeholderColor="gray"
+                returnKeyType="done"
+                secure={true}
+                text={password}
+                variant="outline"
+                on:textChange={(e) => (password = e['value'])} />
+        </stacklayout>
+    </scrollview>
 </gesturerootview>
