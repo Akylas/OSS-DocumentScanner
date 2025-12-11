7e11b748ae16df36b5fec284211af62876f8eae4
farfromrefug <dev@akylas.fr>
2025-12-11 21:35:24 +0100

    chore: new zip plugin (to be opensourced)

--- app/utils/zip.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ZipUtils {
-    zipFolder(sourcePath: string, zipPath: string): void;
-    unzipFile(zipPath: string, destPath: string): void;
-}
-
-export const zipUtils: ZipUtils;
